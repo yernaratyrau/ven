93c7b02fcc58c952da5518c6cf7eb2e7866ae021
unknown <yernar2231995@gmail.com>
2025-06-07 18:14:56 +0500

    vers1

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'GAY')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def tt(name):
+    print("Almat")
